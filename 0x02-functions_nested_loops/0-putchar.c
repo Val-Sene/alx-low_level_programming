2636284f942baaf7b602b20f2d2f2d8572255393
val-sene <adjeivalentine11@gmail.com>
2022-07-05 16:17:04 -0700

    putchar file add

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<unistd.h>
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
