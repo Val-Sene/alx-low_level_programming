33a016e50ff0f1d4e6cf972368d34c845b3e7ca0
val-sene <adjeivalentine11@gmail.com>
2022-07-06 15:05:47 -0700

    add file added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include<unistd.h>
+#include "main.h"
+/**
+ *add - add two numbers
+ *
+ * *@i:The integer value
+ * *@a:Another value
+ *
+ * Return: The sum value
+ */
+int add(int i, int a)
+{
+	return (i + a);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum;
+
+	sum = add(98, 2);
+	printf("%d", sum);
+	return (0);
+}
