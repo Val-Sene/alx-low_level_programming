f65c0c05ff12029dc9d8ea07224a5510b32f530b
val-sene <adjeivalentine11@gmail.com>
2022-07-11 09:48:57 -0700

    0-reset_to_98 file added

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 -  update the pointer value to 98
+ *
+ * @n: integer value
+ *
+ * Return: no return value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is a readme for thsi directory
+0-reset_to_98
