a1fbe13194b5ed7354c1c0a9f72838ebaa8b5af7
val-sene <adjeivalentine11@gmail.com>
2022-07-20 18:31:04 -0700

    _print_rev_recursion

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_print_rev_recursion("\nPuts with recursion");
+	return (0);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,3 @@
 This is a readme for this directory
 0-_puts_recursion
+1- _print_rev_recursion
