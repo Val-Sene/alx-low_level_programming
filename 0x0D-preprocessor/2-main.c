6bd671f13305055eea7587781b7462f2c26c296a
val-sene <adjeivalentine11@gmail.com>
2022-08-02 14:46:58 -0700

    2-main.c file added

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the name of the file used in the compiling process
+ *
+ * Return: Always success
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
