6684f55184ba7e7fddc308e8a1764735bdd62691
val-sene <adjeivalentine11@gmail.com>
2022-08-12 05:45:26 -0700

    1-list_len file add

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,12 +3,11 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string
- * @len: length  of string
+ * @str: string - (malloc'ed string)
+ * @len: length  of the string
  * @next: points to next node
  *
  * Description: singly linked list node structure
- *
  */
 typedef struct list_s
 {
@@ -17,8 +16,8 @@ typedef struct list_s
 	struct node *next;
 } list_t;
 
-int _putchar(char c);
 size_t print_list(const list_t *h);
-
+size_t list_len(const list_t *h);
+int _putchar(char c);
 
 #endif
