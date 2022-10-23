a7acbba40d55b39f8d43975262f54f930f127583
Val-Sene <62505251+Val-Sene@users.noreply.github.com>
2022-10-23 18:30:59 +0000

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,13 @@
  * @size: size of the hash table
  * Return: the created hash table, or NULL if function fails
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	hash_node_t **array;
 	unsigned long int i;
 
-	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
